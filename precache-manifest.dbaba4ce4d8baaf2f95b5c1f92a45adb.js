8b47236708ada4a61caca8239bd02fd9f73cbeba
ipswebdev <sawant.pranay.15ee1102@gmail.com>
2019-07-28 11:25:45 +0530

    committing source files

---
+++ precache-manifest.dbaba4ce4d8baaf2f95b5c1f92a45adb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0af2996e9d03b30b4e5edccde04ffb2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "44a5d163d999b128c18e",
+    "url": "/static/css/main.75b4c84b.chunk.css"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "44a5d163d999b128c18e",
+    "url": "/static/js/main.f95cdd5b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
