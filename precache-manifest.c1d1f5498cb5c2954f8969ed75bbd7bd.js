e82a42c6ac0b5fd660c31aa3ee2bfc3885ccacbc
ipswebdev <sawant.pranay.15ee1102@gmail.com>
2019-07-29 10:56:40 +0530

    Updates

---
+++ precache-manifest.c1d1f5498cb5c2954f8969ed75bbd7bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16a1dba6ad802b84bc507f7d1d89b661",
+    "url": "/RoboFriendsApp/index.html"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/RoboFriendsApp/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "a3f3ccc3b105ac72d0ca",
+    "url": "/RoboFriendsApp/static/css/main.2d07ead4.chunk.css"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/RoboFriendsApp/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "a3f3ccc3b105ac72d0ca",
+    "url": "/RoboFriendsApp/static/js/main.c59008f2.chunk.js"
+  },
+  {
+    "revision": "40cbab871f78f77db683",
+    "url": "/RoboFriendsApp/static/js/runtime~main.9967c245.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriendsApp/static/media/SEGA.12a3db8b.woff"
+  }
+]);
